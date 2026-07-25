1e93814e8940f61446a4dd9a02afd141daba6a4d
Vigneswaran Tharanesh <135099337+Tharanesh-github@users.noreply.github.com>
2026-07-25 16:06:15 +0530

    Implement command line arguments for node configuration
    Added command line argument parsing for port and peers.

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
+	"strings"
 
+	"github.com/tharanesh/go-kv/internal/raft"
 	"github.com/tharanesh/go-kv/internal/rpc"
 	"github.com/tharanesh/go-kv/internal/storage"
 )
 
 func main() {
-	log.Println("Starting GO-KV Node...")
+	// Parse command line arguments to allow multiple nodes on one computer
+	port := flag.String("port", "8080", "The port this node will run on")
+	peersFlag := flag.String("peers", "", "Comma-separated list of peer ports (e.g., 8081,8082)")
+	flag.Parse()
+
+	var peers []string
+	if *peersFlag != "" {
+		peers = strings.Split(*peersFlag, ",")
+	}
+
+	log.Printf("Starting GO-KV Node on port %s...", *port)
 
 	// 1. Boot up the Storage Engine
-	log.Println("Initializing thread-safe storage engine...")
 	db := storage.NewKVStore()
 
-	// 2. Start the RPC/HTTP Server on port 8080
-	port := "8080"
-	log.Printf("Starting network listener on port %s...", port)
-	
-	err := rpc.Start(port, db)
+	// 2. Boot up the Raft Consensus Brain
+	raftNode := raft.NewNode(*port, peers)
+	go raftNode.RunElectionTimer()
+
+	// 3. Start the RPC/HTTP Server
+	log.Printf("Starting network listener on port %s...", *port)
+	err := rpc.Start(*port, db, raftNode)
 	if err != nil {
 		log.Fatalf("Server crashed: %s", err)
 	}
